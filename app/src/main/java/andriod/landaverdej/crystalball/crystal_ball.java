69a8fb1698f189f09a26c0d90a79665db7e25f09
justin landaverde <justlandaverde28@gmail.com>
2014-09-17 15:05:52 -0700

    /got to add text to my app

--- app/src/main/java/andriod/landaverdej/crystalball/crystal_ball.java
+++ app/src/main/java/andriod/landaverdej/crystalball/crystal_ball.java
@@ -4,33 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class crystal_ball extends Activity {
 
+    private TextView answerText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crystal_ball);
+
+    answerText = (TextView) findViewById(R.id.answerText);
+        answerText.setText("reply is hazy, try again tomorrow");
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.crystal_ball, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
